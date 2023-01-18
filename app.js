38f7e9336819b732ccd5fe97814da09d04d10e73
Sophia Okosodo <sophieokosodo@gmail.com>
2023-01-18 22:51:19 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import { dbConnect } from './config/dbConnect.js';
+import userRoute from './routes/userRoute.js';
+dotenv.config();
+dbConnect();
+
+const app = express();
+//middleware
+app.use(express.json())
+//routes
+
+
+
+//login
+app.use("/api/v1/users", userRoute)
+
+
+
+
+
+//error handlers
+//listen server
+
+const PORT = process.env.Port || 3000;
+app.listen(PORT,console.log(`Server is running at ${PORT}`))
